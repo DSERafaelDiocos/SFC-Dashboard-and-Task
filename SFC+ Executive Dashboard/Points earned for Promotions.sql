b7aa61a601573f5279b04951fe19696670886317
RDiocos <thegodbless041@gmail.com>
2024-12-04 08:33:42 +0800

    Update and rename Earn Promotion.sql to Points earned for Promotions.sql

---
+++ SFC+ Executive Dashboard/Points earned for Promotions.sql
@@ -0,0 +1,55 @@
+-- Balance as of previous week
+SELECT 
+    'Balance as of ' || TO_CHAR(DATE_TRUNC('WEEK', CURRENT_TIMESTAMP) - INTERVAL '1 DAY', 'MM/DD/YYYY') AS Description,
+    TO_CHAR(SUM(points), 'FM999,999,999,999') AS Points
+FROM "BPAY_SFCRWDS_transactions" t        
+    INNER JOIN "BPAY_SFCRWDS_accounts" a ON a.id = t.account_id
+WHERE        
+    t.type || '-' || t.source = 'EARN-PROMOTION'    
+    AND account_id != '084c35ba-4e90-4cf9-96e6-ffb399c4646b'    -- "INTERNAL-TRANSFER"
+    -- AND CAST(CONVERT_TIMEZONE('America/Los_Angeles', "timestamp") AS DATE) < DATE_TRUNC('WEEK', CURRENT_TIMESTAMP)
+    AND CAST(CONVERT_TIMEZONE('America/Los_Angeles', "timestamp") AS DATE) < CAST(CONVERT_TIMEZONE('America/Los_Angeles', DATE_TRUNC('WEEK', CURRENT_TIMESTAMP)) AS DATE)
+
+UNION ALL        
+
+-- Points for specific date (:sfcdate)
+SELECT 
+    TO_CHAR(CAST(CONVERT_TIMEZONE('America/Los_Angeles', "timestamp") AS DATE), 'MM/DD/YYYY') AS Description,        
+    TO_CHAR(SUM(points), 'FM999,999,999,999') AS Points
+FROM 
+    "BPAY_SFCRWDS_transactions" t        
+    INNER JOIN "BPAY_SFCRWDS_accounts" a 
+        ON a.id = t.account_id    
+WHERE        
+    t.type || '-' || t.source = 'EARN-PROMOTION'    
+    AND account_id != '084c35ba-4e90-4cf9-96e6-ffb399c4646b'    -- "INTERNAL-TRANSFER"
+    AND CAST(CONVERT_TIMEZONE('America/Los_Angeles', "timestamp") AS DATE) > CAST(CONVERT_TIMEZONE('America/Los_Angeles', DATE_TRUNC('WEEK', CURRENT_TIMESTAMP)) AS DATE)
+    -- AND TO_CHAR(CAST(CONVERT_TIMEZONE('America/Los_Angeles', "timestamp") AS DATE), 'MM/DD/YYYY') > DATE_TRUNC('WEEK', CURRENT_TIMESTAMP)
+    AND CAST(CONVERT_TIMEZONE('America/Los_Angeles', "timestamp") AS DATE) != CAST(CONVERT_TIMEZONE('America/Los_Angeles', CURRENT_TIMESTAMP) AS DATE)
+    -- AND TO_CHAR(CAST(CONVERT_TIMEZONE('America/Los_Angeles', "timestamp") AS DATE), 'MM/DD/YYYY') <= CURRENT_DATE  -- Exclude today's date
+GROUP BY 
+    TO_CHAR(CAST(CONVERT_TIMEZONE('America/Los_Angeles', "timestamp") AS DATE), 'MM/DD/YYYY')
+
+
+UNION ALL
+
+
+-- Total points as of current week
+SELECT
+   'Total' AS Description,
+    TO_CHAR(SUM(points), 'FM999,999,999,999') AS Points
+FROM "BPAY_SFCRWDS_transactions" t        
+    INNER JOIN "BPAY_SFCRWDS_accounts" a ON a.id = t.account_id
+WHERE        
+    t.type || '-' || t.source = 'EARN-PROMOTION'    
+    AND account_id != '084c35ba-4e90-4cf9-96e6-ffb399c4646b'
+    AND CAST(CONVERT_TIMEZONE('America/Los_Angeles', "timestamp") AS DATE) != CAST(CONVERT_TIMEZONE('America/Los_Angeles', CURRENT_TIMESTAMP) AS DATE)
+    -- AND CAST(CONVERT_TIMEZONE('America/Los_Angeles', "timestamp") AS DATE) >= DATE_TRUNC('WEEK', CURRENT_DATE) 
+-- GROUP BY        
+--     CAST(CONVERT_TIMEZONE('America/Los_Angeles', "timestamp") AS DATE)
+ORDER BY 
+    CASE 
+        WHEN Description LIKE 'Balance%' THEN 1
+        WHEN Description LIKE 'Total%' THEN 3
+        ELSE 2
+    END, Description ASC;
